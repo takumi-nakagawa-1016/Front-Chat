838be795383df67bc051a3ae4ae567e804e6b4c4
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-06-04 11:05:43 +0900

    Next.jsにFirebaseのセットアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-
+import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
+initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(getApp())
   return (
     <ChakraProvider>
       <Component {...pageProps} />
