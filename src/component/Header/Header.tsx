02b61a4994edb8980317ad9650d47ed4964a9a14
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-06-04 13:12:19 +0900

    サインアウト機能の実装

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,52 @@
+import {Button, chakra, Container, Heading, useToast} from '@chakra-ui/react'
+import {useAuthContext} from "@src/feature/auth/provider/AuthProvider";
+import {getAuth, signOut} from "@firebase/auth";
+import {FirebaseError} from "@firebase/app";
+import {useState} from "react";
+import {useRouter} from "next/router";
+
+export const Header = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const toast = useToast()
+    const { push } = useRouter()
+
+    const {user} = useAuthContext()
+    const handleSignOut = async () => {
+        setIsLoading(true)
+        try {
+            const auth = getAuth()
+            await signOut(auth)
+            toast({
+                title: 'ログアウトしました。',
+                status: 'success',
+                position: 'top',
+            })
+            push('/signin')
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                console.log(e)
+            }
+        } finally {
+        setIsLoading(false)
+    }
+    }
+    return (
+        <chakra.header py={4} bgColor={'blue.600'}>
+            <Container maxW={'container.lg'}>
+                <Heading color={'white'}>
+                    {user ? (
+                        <Button
+                            colorScheme={'teal'}
+                            onClick={handleSignOut}
+                            isLoading={isLoading}
+                        >
+                            サインアウト
+                        </Button>
+                    ) : (
+                        'ログアウト中'
+                    )}
+                </Heading>
+            </Container>
+        </chakra.header>
+    )
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import {ChakraProvider} from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
-import { getApp } from 'firebase/app'
+import {AuthProvider} from "@src/feature/auth/provider/AuthProvider";
+import {Header} from "@src/component/Header/Header";
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    return (
+        <ChakraProvider>
+            <AuthProvider>
+                <Header />
+                <Component {...pageProps} />
+            </AuthProvider>
+        </ChakraProvider>
+    );
 }
 
 export default MyApp
