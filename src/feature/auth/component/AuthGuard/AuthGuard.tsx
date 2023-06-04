0ad552a713f9be5eac900d2fb78e4693b97e4f83
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-06-04 14:34:42 +0900

    yarn format

--- src/feature/auth/component/AuthGuard/AuthGuard.tsx
+++ src/feature/auth/component/AuthGuard/AuthGuard.tsx
@@ -1,24 +1,24 @@
 import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
-import { useRouter } from 'next/router'
+import { useRouter } from '@src/hooks/useRouter/useRouter'
 import type { ReactNode } from 'react'
 import { Box } from '@chakra-ui/react'
 
 type Props = {
-    children: ReactNode
+  children: ReactNode
 }
 
 export const AuthGuard = ({ children }: Props) => {
-    const { user } = useAuthContext()
-    const { push } = useRouter()
+  const { user } = useAuthContext()
+  const { push } = useRouter()
 
-    if (typeof user === 'undefined') {
-        return <Box>読み込み中...</Box>
-    }
+  if (typeof user === 'undefined') {
+    return <Box>読み込み中...</Box>
+  }
 
-    if (user === null) {
-        push('/signin')
-        return null
-    }
+  if (user === null) {
+    push((path) => path.signin.$url())
+    return null
+  }
 
-    return <>{children}</>
+  return <>{children}</>
 }
